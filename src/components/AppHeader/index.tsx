456844beb0557f81b3e5d66fb7d223865f0629cc
CodeSandbox <support@codesandbox.io>
2025-05-30 08:45:06 +0000

    Sandbox snapshot commit

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -20,87 +20,95 @@ const typoStyle = {
   display: "flex",
   alignContent: "center",
   justifyContent: "center",
-  lineHeight: 1
+  lineHeight: 1,
 };
 
 const AppBar = styled(MuiAppBar)<AppBarProps>(({ theme }) => ({
   zIndex: theme.zIndex.drawer + 1,
   backgroundColor: theme.palette.common.black,
   color: theme.palette.common.white,
-  height: theme.tokens.header.height
+  height: theme.tokens.header.height,
 }));
 
-const AppHeader = React.forwardRef<HTMLDivElement, AppHeaderProps>((props: AppHeaderProps, ref) => {
-  const { user, pageTitle } = props;
-  const { t } = useTranslation("app");
-  const theme = useTheme();
+const AppHeader = React.forwardRef<HTMLDivElement, AppHeaderProps>(
+  (props: AppHeaderProps, ref) => {
+    const { user, pageTitle } = props;
+    const { t } = useTranslation("app");
+    const theme = useTheme();
 
+    const [count, setCount] = useState(0);
+    const hours = 1;
+    const minutes = hours * 60;
+    const seconds = minutes * 60;
 
-  const [count, setCount] = useState(0);
-  const hours = 1;
-  const minutes = hours * 60;
-  const seconds = minutes * 60;
+    const countdown = useMemo(() => seconds - count, [seconds, count]);
+    const countdownMinutes = useMemo(
+      () => `${~~(countdown / 60)}`.padStart(2, "0"),
+      [countdown]
+    );
+    const countdownSeconds = useMemo(
+      () => (countdown % 60).toFixed(0).padStart(2, "0"),
+      [countdown]
+    );
 
- const countdown = useMemo(() => seconds - count, [seconds,count])
- const countdownMinutes = useMemo(() => `${~~(countdown / 60)}`.padStart(2, "0"), [countdown])
- const countdownSeconds = useMemo(() => (countdown % 60).toFixed(0).padStart(2, "0"), [countdown])
+    useEffect(() => {
+      if (countdown <= 0) return;
+      const interval = setInterval(() => {
+        setCount((c) => c + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }, [countdown]);
 
+    return (
+      <AppBar ref={ref} position="fixed" sx={{ width: "100vw" }}>
+        <Toolbar sx={{ background: "#08140C 0% 0% no-repeat padding-box" }}>
+          <Box sx={{ width: "100%", flexDirection: "row", display: "flex" }}>
+            <Box>
+              <Typography variant="h6" component="div" color="primary">
+                {countdownMinutes}:{countdownSeconds}
+              </Typography>
+            </Box>
+            <Box sx={{ width: 20, height: 20, flex: 1 }} />
+            <Box sx={{ flex: 2 }}>
+              <Typography
+                sx={{
+                  ...typoStyle,
+                  color: theme.palette.primary.main,
+                  mb: theme.spacing(0.5),
+                }}
+                variant="h6"
+                component="div"
+              >
+                {t("appTitle").toLocaleUpperCase()}
+              </Typography>
+              <Typography
+                sx={{ ...typoStyle }}
+                variant="overline"
+                component="div"
+                noWrap
+              >
+                {pageTitle.toLocaleUpperCase()}
+              </Typography>
+            </Box>
 
-  useEffect(() => {
-    if (countdown <= 0) return;
-  const interval = setInterval(() => {
-    setCount((c) => c + 1);
-  }, 1000);
-    return () => clearInterval(interval); 
-  }, [countdown]);
-
-  return (
-    <AppBar ref={ref} position="fixed" sx={{ width: "100vw" }}>
-      <Toolbar sx={{ background: "#08140C 0% 0% no-repeat padding-box" }}>
-        <Box sx={{ width: "100%", flexDirection: "row", display: "flex" }}>
-          <Box>
-            <Typography variant="h6" component="div" color="primary">
-              {countdownMinutes}:{countdownSeconds}
-            </Typography>
-          
-            
-          </Box>
-          <Box sx={{ width: 20, height: 20, flex: 1 }} />
-          <Box sx={{ flex: 2 }}>
-            <Typography
+            <Box
               sx={{
-                ...typoStyle,
-                color: theme.palette.primary.main,
-                mb: theme.spacing(0.5)
+                flex: 1,
+                justifyContent: "flex-end",
+                display: "flex",
+                gap: "20px",
               }}
-              variant="h6"
-              component="div"
             >
-              {t("appTitle").toLocaleUpperCase()}
-            </Typography>
-            <Typography
-              sx={{ ...typoStyle }}
-              variant="overline"
-              component="div"
-              noWrap
-            >
-              {pageTitle.toLocaleUpperCase()}
-            </Typography>
+              <LanguageSwitcher />
+              <Grow in={!!user?.eMail}>
+                <div>{user?.eMail && <AvatarMenu user={user} />}</div>
+              </Grow>
+            </Box>
           </Box>
-      
-           
-         <Box sx={{ flex: 1, justifyContent: "flex-end", display: "flex" , gap:"20px" }}>
-         <LanguageSwitcher />
-         <Grow in={ !!user?.eMail}>
-          <div>
-            {user?.eMail && <AvatarMenu user={user} />}
-          </div>
-         </Grow>
-          </Box>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
-});
+        </Toolbar>
+      </AppBar>
+    );
+  }
+);
 
 export default AppHeader;
